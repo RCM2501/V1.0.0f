cf8247b2532e954c0d68a7c0905602eaedb69c4e
RCM2501 <ricomourik@gmail.com>
2026-05-17 15:56:16 +0200

    modified:   Hobbies.html 	modified:   Home.html 	renamed:    Vragenlijst.html -> Luisteren.html 	modified:   Stijl.css 	modified:   index.html 	modified:   script.js

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
 const loginForm = document.getElementById('loginForm');
 const message = document.getElementById('message');
 
+let hobbiesLoggedIn = false;
+
 loginForm.addEventListener('submit', function(event) {
   event.preventDefault();
 
   const email = document.getElementById('email').value;
   const password = document.getElementById('password').value;
 
-  // Simpele demo login
   if (email === 'test@mail.com' && password === '123456') {
-    message.textContent = 'Succesvol ingelogd!';
+    message.textContent = 'Succesvol ingelogd';
+    hobbiesLoggedIn = true;
   } else {
     message.textContent = 'Verkeerde logingegevens';
   }
